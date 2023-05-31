363f447828a6c495d9c949d24342642b28d7db9f
Matias Santillan <sntlln.93@gmail.com>
2023-05-31 16:38:22 -0300

    Linted

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
-const isDev = process.env.NODE_ENV !== "production";
-const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
 
 const createWelcomeWindow = () => {
   const welcomeWindow = new BrowserWindow({
@@ -13,32 +13,31 @@ const createWelcomeWindow = () => {
     // y: 0,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-    },
+      contextIsolation: false
+    }
   });
 
   welcomeWindow.setAspectRatio(9 / 16);
 
   if (isDev) {
     welcomeWindow.webContents.openDevTools();
+  } else {
+    welcomeWindow.setAlwaysOnTop(true, 'screen');
   }
 
-  //TODO: remove
-  // welcomeWindow.setAlwaysOnTop(true, "screen");
-
-  welcomeWindow.loadFile("renderer/index.html");
+  welcomeWindow.loadFile('renderer/index.html');
 };
 
 app.whenReady().then(() => {
   createWelcomeWindow();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWelcomeWindow();
     }
   });
 });
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   if (!isMac) app.quit();
 });
